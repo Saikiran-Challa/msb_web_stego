1b245a26dc9a23b421b465b4d0eeb9f2828a61fb
Saikiran-Challa <challasaikiran124@gmail.com>
2025-08-03 22:20:51 +0530

    Fix host and port for Render deployment

--- app.py
+++ app.py
@@ -51,4 +51,7 @@ def decrypt():
     return render_template("index.html", decrypted_text=hidden_message)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+   import os
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port, debug=True)
+
